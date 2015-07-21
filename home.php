3a9bf4045f6fca658ff5504b1c0845116ee0d2cb
myapos <myapos@yahoo.com>
2015-07-21 23:20:51 +0300

    Fixed footer with social icons
    Fixed footer with social icons

--- home.php
+++ home.php
@@ -23,11 +23,15 @@ session_start();
         <!-- Optional jQuery UI theme -->
         <link rel="stylesheet" href="css/jquery-ui.min.css">
   
+  			<!--css for sharing buttons-->
+  			<link rel="stylesheet" href="css/rrssb.css" />
+  			
         <script src="js/jquery-1.11.3.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
   
         <!-- Minified jQuery  minified libs-->
         <script src="js/jquery-ui.min.js"></script>  
+
 
 	<link rel="stylesheet" href="css/mycss.css">
         
@@ -367,41 +371,56 @@ echo "Welcome, ".$role." ".$_SESSION["myusername"];
      <div class="container">
   
             <div class="row footer">
-                <div class="col-xs-4">
-                    <ul class="nav navbar-nav">
-				<li class="active">
-					<a href="http://getbootstrap.com/" target="_blank">
-						<img class="img-responsive center-block" src="images/logos/logoBootstrap.png" alt="Bootstrap">
-					</a>
-				</li>
-				<li class="active">
-					<a href="https://ellak.gr/" target="_blank">
-						<img class="img-responsive center-block" src="images/logos/logoEllak.png" alt="Bootstrap"></a>
-
-				</li>
-				<li class="active">
-					<a href="https://ma.ellak.gr/" target="_blank">
-						<img class="img-responsive center-block" src="images/logos/logoMonadesAristeias.png" alt="Bootstrap">
-					</a>
-				</li>
-
-				<li class="active">
-					<a href="http://www.uoc.gr/" target="_blank">
-						<img class="img-responsive center-block" src="images/logos/logoPK.png" alt="Bootstrap">
-					</a>
-				</li>
-	 	    </ul>
-                </div>
-                <div class="col-xs-4">
-                    <p id="footerdevelopment">Website development <a href="http://myapos.oncrete.gr" target="_blank">Apostolakis Myron</a></p>
-                </div>
-                <div class="col-xs-4">
-                    <p id="footercodeschool">&COPY; Monades Aristeias Logismikou - <a href="https://ma.ellak.gr/edu/course/view.php?id=49" target="_blank">2nd Code School</a></p>
-                </div>
+                <div class="col-xs-5">
+           			<ul class="nav navbar-nav rrssb-buttons clearfix">
+							<li class="active">
+								<a href="http://getbootstrap.com/" target="_blank">
+									<img class="img-responsive center-block" src="images/logos/logoBootstrap.png" alt="Bootstrap">
+								</a>
+							</li>
+							<li class="active">
+								<a href="https://ellak.gr/" target="_blank">
+									<img class="img-responsive center-block" src="images/logos/logoEllak.png" alt="Bootstrap"></a>
+							</li>
+							<li class="active">
+								<a href="https://ma.ellak.gr/" target="_blank">
+									<img class="img-responsive center-block" src="images/logos/logoMonadesAristeias.png" alt="Bootstrap">
+								</a>
+							</li>
+							<li class="active">
+								<a href="http://www.uoc.gr/" target="_blank">
+									<img class="img-responsive center-block" src="images/logos/logoPK.png" alt="Bootstrap">
+								</a>
+							</li>
+  							<li class="rrssb-facebook">
+    							<a href="https://www.facebook.com/sharer/sharer.php?u=http://ellaksrv.datacenter.uoc.gr/~user738/greekbeaches/home.php" class="popup">
+      						<span class="rrssb-icon">
+        							<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="29" height="29" viewBox="0 0 29 29">
+        			 					<path d="M26.4 0H2.6C1.714 0 0 1.715 0 2.6v23.8c0 .884 1.715 2.6 2.6 2.6h12.393V17.988h-3.996v-3.98h3.997v-3.062c0-3.746 2.835-5.97 6.177-5.97 1.6 0 2.444.173 2.845.226v3.792H21.18c-1.817 0-2.156.9-2.156 2.168v2.847h5.045l-.66 3.978h-4.386V29H26.4c.884 0 2.6-1.716 2.6-2.6V2.6c0-.885-1.716-2.6-2.6-2.6z"
+          							class="cls-2" fill-rule="evenodd"/>
+        							</svg>
+      						</span>
+    							</a>
+  							</li>  
+  							<li class="rrssb-twitter">
+   						 	<a href="https://twitter.com/intent/tweet?text=http://ellaksrv.datacenter.uoc.gr/~user738/greekbeaches/home.php"
+    							class="popup">
+      							<span class="rrssb-icon"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28"><path d="M24.253 8.756C24.69 17.08 18.297 24.182 9.97 24.62c-3.122.162-6.22-.646-8.86-2.32 2.702.18 5.375-.648 7.507-2.32-2.072-.248-3.818-1.662-4.49-3.64.802.13 1.62.077 2.4-.154-2.482-.466-4.312-2.586-4.412-5.11.688.276 1.426.408 2.168.387-2.135-1.65-2.73-4.62-1.394-6.965C5.574 7.816 9.54 9.84 13.802 10.07c-.842-2.738.694-5.64 3.434-6.48 2.018-.624 4.212.043 5.546 1.682 1.186-.213 2.318-.662 3.33-1.317-.386 1.256-1.248 2.312-2.4 2.942 1.048-.106 2.07-.394 3.02-.85-.458 1.182-1.343 2.15-2.48 2.71z"/></svg></span>
+   			 				</a>
+  							</li>
+  							</ul>
+            		</div>
+                	<div class="col-xs-4">
+                	    <p id="footerdevelopment">Website development <a href="http://myapos.oncrete.gr" target="_blank">Apostolakis Myron</a></p>
+                	</div>
+                	<div class="col-xs-3">
+                  	  <p id="footercodeschool">&COPY; Monades Aristeias Logismikou - <a href="https://ma.ellak.gr/edu/course/view.php?id=49" target="_blank">2nd Code School</a></p>
+                	</div>
             </div>      
-  
-    </div>        
+     </div> 
+     
+            
 </div> <!--end of big_wrapper-->     
-        
-    </body>
+<script src="js/rrssb.min.js"></script>        
+</body>
 </html>
